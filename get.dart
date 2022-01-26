3ec11df17deb33299d7151334484a58883f10f18
yeonwoo13 <kangyw2942@gmail.com>
2022-01-26 11:36:25 +0900

    init

---
+++ get.dart
@@ -0,0 +1,6 @@
+
+void main(){
+  Response response =await http.get(url, body: {'name': 'doodle', 'color': 'blue'});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+}
